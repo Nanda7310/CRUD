99b75a295bb34216306d4f97416609286983662f
nandu <nandakumar7310@gmail.com>
2024-05-16 14:46:56 +0530

    first

---
+++ src/users/ViewUser.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ViewUser = () => {
+  return (
+    <div className='container'>
+    <div className='row'>
+        <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+            <h2 className='text-center m-4'>Register User</h2>
+            <div className='card'>
+                <div className='card-header'>
+                    Details of user id:
+                    <ul className='list-group list-group-flush'>
+                        <li className='list-group-item'>
+                            <b>Name:</b>
+                        </li>
+                        <li className='list-group-item'>
+                            <b>UserName:</b>
+                        </li>
+                        <li className='list-group-item'>
+                            <b>Email:</b>
+                        </li>
+                    </ul>
+                </div>
+                <Link className="btn btn-primary my-2" to={"/"}>Back To Home</Link>
+            </div>
+            </div>
+            </div>
+
+</div>  )
+}
+
+export default ViewUser
